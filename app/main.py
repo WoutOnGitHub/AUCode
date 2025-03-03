a009810224dae238aa36f2754ad33ee59c34316e
Wolter van der Hoef <woltervanderhoef@gmail.com>
2025-03-03 21:22:43 +0100

    Add homepage and auth apge

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-# app/main.py
-from flask import redirect
+from datetime import datetime
+
+from flask import render_template
 
 from config.database import create_tables
 
@@ -8,7 +9,17 @@ def init_app(app):
     # Ensure database tables exist
     create_tables()
 
+    # Register blueprints
+    from app.routes.auth import auth_bp
+
+    app.register_blueprint(auth_bp)
+
+    # Add context processor for templates
+    @app.context_processor
+    def inject_now():
+        return {"now": datetime.utcnow()}
+
     # Define root route
     @app.route("/")
     def index():
-        return "Hello world!"
+        return render_template("base.html")
